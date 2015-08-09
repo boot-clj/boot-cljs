d479f10935be321232e2363e2ae3e9cc515a81af
Juho Teperi <juho.teperi@iki.fi>
2015-08-09 10:31:19 +0300

    Use Cljs API for output file dependency graph

--- src/adzerk/boot_cljs/impl.clj
+++ src/adzerk/boot_cljs/impl.clj
@@ -1,35 +1,35 @@
 (ns adzerk.boot-cljs.impl
   (:require [boot.file :as file]
             [boot.kahnsort :as kahn]
-            [cljs.analyzer.api :refer [empty-state default-warning-handler warning-enabled?]]
-            [cljs.build.api :refer [build inputs]]
+            [cljs.analyzer.api :as ana-api :refer [empty-state default-warning-handler warning-enabled?]]
+            [cljs.build.api :refer [build inputs target-file-for-cljs-ns]]
             [clojure.java.io :as io]))
 
 ; Because this ns is loaded in pod, it's private to one cljs task.
 ; Compiler env is a atom.
 (def ^:private stored-env (empty-state))
 
-(defn dep-order
-  "Returns a seq of paths for all js files created by CLJS compiler, relative
-  to the :output-to compiled JS file, and in dependency order."
-  [env {:keys [output-dir]}]
-  ; FIXME: Uses cljs compiler private data
-  (let [cljs-nses (:cljs.compiler/compiled-cljs @env)
-        js-nses   (reduce-kv (fn [xs k v]
-                               (assoc xs (str output-dir "/" (:file v)) v))
-                             {}
-                             (:js-dependency-index @env))
-        all-nses  (reduce-kv (fn [xs k v]
-                               (reduce #(assoc %1 (str %2) (str k)) xs (:provides v)))
-                             {}
-                             (merge js-nses cljs-nses))]
-    (->> cljs-nses
-         (reduce-kv (fn [xs k v]
-                      (assoc xs k (set (keep (comp all-nses str) (:requires v)))))
-                    {})
-         kahn/topo-sort
-         reverse
-         (map #(.getPath (file/relative-to (.getParentFile (io/file output-dir)) (io/file %)))))))
+(defn ns-dependencies
+  "Given a namespace as a symbol return list of namespaces required by the namespace."
+  ; ([ns] (ns-dependencies env/*compiler* ns))
+  ([state ns]
+   (vals (:requires (ana-api/find-ns state ns)))))
+
+(defn cljs-depdendency-graph [state]
+  (let [all-ns (ana-api/all-ns state)
+        all-ns-set (set all-ns)]
+    (->> all-ns
+         (reduce (fn [acc n]
+                   (assoc acc n (->> (ns-dependencies state n)
+                                     (keep all-ns-set)
+                                     (set))))
+                 {}))))
+
+(defn dep-order [env]
+  (->> (cljs-depdendency-graph env)
+       (kahn/topo-sort)
+       reverse
+       (map #(.getPath (target-file-for-cljs-ns %)))))
 
 (defn compile-cljs
   "Given a seq of directories containing CLJS source files and compiler options
@@ -48,4 +48,4 @@
       (assoc opts :warning-handlers [default-warning-handler handler])
       stored-env)
     {:warnings  @counter
-     :dep-order (dep-order stored-env opts)}))
+     :dep-order (dep-order stored-env)}))
